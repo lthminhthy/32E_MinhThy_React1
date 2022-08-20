5f93c6d8c18058e8c6b8887caa16699e9a3e4b5d
lthminhthy <lthminhthy@gmail.com>
2022-08-20 10:10:55 +0700

    fix code

--- src/BaiTapThucHanhLayout/index.jsx
+++ src/BaiTapThucHanhLayout/index.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
-import Banner from './components/Banner'
+// import Banner from './components/Banner'
 import Body from './components/Body'
 import Footer from './components/Footer'
 import Header from './components/Header'
-import Item from './components/Item'
+// import Item from './components/Item'
 
 export default class BaiTapThucHanhLayout extends Component {
   render() {
